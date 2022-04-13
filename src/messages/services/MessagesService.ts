0742ddbd54275ed0f6e36e6123e62307ba9ce97b
Esteban Piga <esteban.piga@globallogic.com>
2022-04-13 16:07:26 -0300

    Add badrequest exception when no text message

--- src/messages/services/MessagesService.ts
+++ src/messages/services/MessagesService.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { Message } from "../models/message";
 import { User } from "../models/user";
 import { MessagesRepository } from "../repositories/messages.repository";
@@ -15,6 +15,10 @@ export class MessagesService {
     }
 
     async create(message: Message, user: User): Promise<Message> {
+        if (!message.text.trim()) {
+            throw new BadRequestException();
+        }
+
         message.moment = new Date();
         message.user = user;
         const newMessage = await this.messagesDB.create(message);
