b6e61c54e1ab53d2827ed060fbf55fc714f52ee3
Esteban Piga <esteban.piga@globallogic.com>
2022-04-20 15:05:26 -0300

    Change mongo image name

--- src/common/config.ts
+++ src/common/config.ts
@@ -1,3 +1,3 @@
 export const JWT_SECRET = "El valor de una idea radica en el uso de la misma!";
-export const MONGO_CONNECTION_STRING = "mongodb://messagesdbuser:messagesdbpassword@mongo:27017/messages";
+export const MONGO_CONNECTION_STRING = "mongodb://messagesdbuser:messagesdbpassword@mongo-messages:27017/messages";
 export const LISTEN_PORT = 9001;
