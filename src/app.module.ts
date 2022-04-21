3673a7c364ae8f28a0c048e75980081ff6969ba0
Esteban Piga <esteban.piga@globallogic.com>
2022-04-21 10:55:21 -0300

    Add dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
-import { MONGO_CONNECTION_STRING, JWT_SECRET } from './common/config';
+import configuration from './common/configuration';
 import { MessagesController } from './messages/controllers/messages.controller';
 import { MessagesModule } from './messages/messages.module';
 import { GetUserMiddleware } from './middlewares/getUser.middleware';
@@ -12,9 +13,23 @@ const middlewares = [GetUserMiddleware];
 @Module({
   imports: [
     ...modules,
-    MongooseModule.forRoot(MONGO_CONNECTION_STRING),
-    JwtModule.register({
-      secret: JWT_SECRET
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return { uri: configService.get('database.connectionString') };
+      },
+      inject: [ConfigService],
+    }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => {
+        return { secret: configService.get('jwtSecret') }
+      },
+      inject: [ConfigService],
     }),
   ],
   exports: [JwtModule]
--- src/common/config.ts
+++
@@ -1,3 +0,0 @@
-export const JWT_SECRET = "El valor de una idea radica en el uso de la misma!";
-export const MONGO_CONNECTION_STRING = "mongodb://messagesdbuser:messagesdbpassword@mongo-messages:27017/messages";
-export const LISTEN_PORT = 9001;
